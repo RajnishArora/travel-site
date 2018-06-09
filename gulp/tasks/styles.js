4774d1c563632f59483e962182a65d4af64965d0
Rajnish Arora <rajarora795@gmail.com>
2018-06-09 23:49:16 +0530

    till lesson 22

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -2,13 +2,16 @@ var gulp=require('gulp');
 var postcss= require('gulp-postcss');
 var autoprefixer= require('autoprefixer');
 var cssvars = require('postcss-simple-vars');
+var mixins = require('postcss-mixins');
 var nested = require('postcss-nested');
 var cssimport = require('postcss-import');
+
+
 
 gulp.task('styles',function(){
      //console.log("Styles gulp called");
      return gulp.src('./app/assets/styles/styles.css')
-     			.pipe(postcss([cssimport, cssvars, nested, autoprefixer]))
+     			.pipe(postcss([cssimport, mixins, cssvars, nested, autoprefixer]))
                 .on('error',function(errorInfo){
                 	console.log(errorInfo.toString());
                 	this.emit('end');
